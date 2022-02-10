0f1d4da7b6b7cacb11850e15c414e500dcc37a4d
rajulucky812 <97225776+rajulucky812@users.noreply.github.com>
2022-02-10 07:58:43 +0530

    Update 05.arithmetic.sh

--- 05.arithmetic.sh
+++ 05.arithmetic.sh
@@ -11,6 +11,5 @@ echo "minus values `expr $a - $b`"
 echo "multiplied by values `expr $a \* $b`"
 echo "devided by values `expr $a / $b`"
 echo "remainder values `expr $a % $b`"
-echo "addition values `expr $a + $b`"
 
 echo "Completed Sucessfully"
